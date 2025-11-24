757a64561d961e551bac1ba755611385bc9ee75e
Agustin Rodriguez <agustinlihuel@gmail.com>
2025-11-24 18:36:31 -0300

    correc imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
-import Item from './components/ItemListContainer'
 import ItemListContainer from './components/ItemListContainer'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <header>
